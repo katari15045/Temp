e6c33218f4cac0c7724cef58b657e65384042ac9
katari Saketh Ram <katari15045@iiitd.ac.in>
2017-03-12 19:48:45 +0530

    Working 1

--- 1/Sample.java
+++ 1/Sample.java
@@ -8,6 +8,8 @@ public class Sample
 	public static void main(String[] args)
 	{
 		takeUserInput();
+		roundTheNumber();
+		System.out.println("Rounded num : " + number);
 	}
 
 	private static void takeUserInput()
@@ -18,5 +20,60 @@ public class Sample
 		number = scanner.nextDouble();
 
 		System.out.println("Your number : " + number );
+	}
+
+	private static void roundTheNumber()
+	{
+		double toAdd;
+
+		if( number == 0.0 )
+		{
+			return;
+		}
+
+		if( number < 0 )
+		{
+			dealNegNum();
+			return;
+		}
+
+		if( number < 100.0 )
+		{
+			number = 100.0;
+			return;
+		}
+
+		if( number % 100.0 == 0.0 )
+		{
+			return;
+		}
+
+		toAdd = 100 - ( number % 100.0 );
+		number = number + toAdd;
+		return;
+	}
+
+	private static void dealNegNum()
+	{
+		double toSubtract;
+		number = -1 * number;
+
+		if( number < 100.0 )
+		{
+			number = 0;
+			return;
+		}
+
+		if( number % 100.0 == 0.0 )
+		{
+			number = -1 * number;
+			return;
+		}
+
+		toSubtract = number % 100;
+		number = number - toSubtract;
+		number = -1 * number;
+
+		return;
 	}
 }
